4fd320c1ab952def620b8a893959010eb2a6ea5e
Bianca de Gobe Andrade <biagob@yahoo.com.br>
2024-12-17 13:13:15 -0300

    Inclusao Dataframe

--- app/services/scraping_service.py
+++ app/services/scraping_service.py
@@ -1,12 +1,22 @@
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 from flask import jsonify, request
 
 def scrape_data(url,headers=None):
     response = requests.get(url,headers=headers)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    data = soup.select('.tb_dados')
-    return [item.text for item in data]
+    soup = BeautifulSoup(response.text, 'html.parser')
+    table = soup.find('table',{'class':'tb_base tb_dados'})
+    rows = table.find_all('tr')
+    data = []
+
+    for row in rows:
+        cells = row.find_all(['th','td'])
+        cells_text = [cell.get_text(strip=True)for cell in cells]
+        data.append(cells_text)
+    df = pd.DataFrame(data)
+    df = pd.DataFrame(data[1:],columns=data[0])
+    return df
 
 def producao(url, headers=None):
     headers = {
@@ -14,7 +24,7 @@ def producao(url, headers=None):
     }
     url = 'http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_02'
     data = scrape_data(url, headers=headers)
-    return jsonify(data)
+    return jsonify(data.to_dict(orient='records'))
 
 def processamento(url, headers=None):
     headers = {
@@ -22,7 +32,7 @@ def processamento(url, headers=None):
     }
     url = 'http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_03'
     data = scrape_data(url, headers=headers)
-    return jsonify(data)
+    return jsonify(data.to_dict(orient='records'))
 
 def comercializacao(url, headers=None):
     headers = {
@@ -30,7 +40,7 @@ def comercializacao(url, headers=None):
     }
     url = 'http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_04'
     data = scrape_data(url, headers=headers)
-    return jsonify(data)
+    return jsonify(data.to_dict(orient='records'))
 
 def importacao(url, headers=None):
     headers = {
@@ -38,7 +48,7 @@ def importacao(url, headers=None):
     }
     url = 'http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_05'
     data = scrape_data(url,headers=headers)
-    return jsonify(data)
+    return jsonify(data.to_dict(orient='records'))
 
 def exportacao(url, headers=None):
     headers = {
@@ -46,4 +56,4 @@ def exportacao(url, headers=None):
     }
     url = 'http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_06'
     data = scrape_data(url,headers=headers)
-    return jsonify(data)
+    return jsonify(data.to_dict(orient='records'))
